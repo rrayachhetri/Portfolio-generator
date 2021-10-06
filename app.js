577475b3537c467b056c03fbf0449721cdb71b0d
rrayachhetri <rrayachhetri@gmail.com>
2021-10-05 18:12:54 -0700

    creating html

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+//profileDataArgs holds the command line argument
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-  for(let i = 0; i < profileDataArr.length; i+=1) {
-  console.log(profileDataArr[i]);
-  };
-
-console.log('==========');
-
-
-//below forEach loop works same as above for loop
-// can't use forEach() if there are no array involved
-profileDataArr.forEach( profileItem => console.log(profileItem));
+// 
+const [name, github] = profileDataArgs;
+// const name = profileDataArgs[0];
+// const GitHub = profileDataArgs[1];
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html>
+  <html lang='en'>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initaila-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+  <body>
+   <h1>${name}</h1>
+   <h2><a href="https://github.com/${github}">GitHub</a></h2>
+   </body>
+   </html>
+   `;
 };
-printProfileData(profileDataArgs);
+// console.log(name, github)
+// console.log(generatePage(name, github));
+const fs = require('fs');
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+  if(err) throw err;
+  console.log('Portfolio Complete! Checkout index.html');
+})
